1df204df8ec67f99cfac88a4dee7e30a9969a489
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-09 22:39:28 -0300

    feat: added date picker and currency input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 'use client'
 
+import TripSearch from "./components/TripSearch"
+
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <TripSearch />
+    </div>
   )
 }
