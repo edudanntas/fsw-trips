22e78827501709e10713de6726d53418eea7ffc2
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-11 14:28:25 -0300

    feat: add quick search component

---
+++ src/app/components/QuickSearch.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Image from 'next/image';
+
+const QuickSearch = () => {
+    return (
+        <div className='container mx-auto p-5'>
+            <div className="flex items-center">
+                <div className='w-full h-[1px] bg-primarylight'></div>
+                <h2 className='px-5 font-medium text-grayPrimary whitespace-nowrap'>Tente pesquisar por</h2>
+                <div className='w-full h-[1px] bg-primarylight'></div>
+            </div>
+
+            <div className='flex w-full justify-between mt-5'>
+                <div className="flex flex-col items-center gap-1">
+                    <Image width={35} height={35} src='/hotel-icon.png' alt='hotel' />
+
+                    <p className='text-sm text-grayPrimary'>Hotel</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Image width={35} height={35} src='/farm-icon.png' alt='fazenda' />
+
+                    <p className='text-sm text-grayPrimary'>Fazenda</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Image width={35} height={35} src='/cottage-icon.png' alt='cottage' />
+
+                    <p className='text-sm text-grayPrimary'>Chalé</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Image width={35} height={35} src='/inn-icon.png' alt='resort' />
+
+                    <p className='text-sm text-grayPrimary'>Resort</p>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default QuickSearch;
