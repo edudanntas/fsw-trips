03050b63a4c6f69d6f99fefe42a5e8d711d1db20
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-16 17:02:55 -0300

    feat: add more date validation on trip

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -24,7 +24,7 @@ interface TripReservationForm {
 }
 
 const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, pricePerDay }: TripReservationProps) => {
-    const { register, handleSubmit, formState: { errors }, control, watch } = useForm<TripReservationForm>()
+    const { register, handleSubmit, formState: { errors }, control, watch, setError } = useForm<TripReservationForm>()
 
     const onSubmit = async (data: TripReservationForm) => {
         const response = await fetch('http://localhost:3000/api/trips/check', {
@@ -38,7 +38,31 @@ const TripReservation = ({ tripId, maxGuests, tripStartDate, tripEndDate, priceP
 
         const res = await response.json()
 
-        console.log({ res });
+        if (res?.error?.code === 'TRIP_ALREADY_RESERVED') {
+            setError("startDate", {
+                type: 'manual',
+                message: "Esta data já está reservada.",
+            });
+
+            setError("endDate", {
+                type: 'manual',
+                message: "Esta data já está reservada.",
+            })
+        }
+
+        if (res?.error?.code === 'INVALID_START_DATE') {
+            setError("startDate", {
+                type: 'manual',
+                message: "Data inválida.",
+            });
+        }
+
+        if (res?.error?.code === 'INVALID_END_DATE') {
+            setError("endDate", {
+                type: 'manual',
+                message: "Data inválida.",
+            });
+        }
     }
 
     const startDate = watch("startDate");
