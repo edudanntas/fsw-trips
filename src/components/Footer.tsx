cd1740dc90f6aa381533fc8bacbdd25ada184a20
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-20 18:51:19 -0300

    style: add more information to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,8 @@ const Footer = () => {
     return (
         <div className='bg-walterwhite p-5 flex flex-col items-center gap-1'>
             <Image src='/logo.svg' width={30} height={32} alt='Logo' />
-            <p className='text-sm text-secondary'>Todos os direitos reservados</p>
+            <p className='text-xs lg:text-sm text-secondary'>2023 Desenvovido por Eduardo</p>
+            <p className='text-xs lg:text-sm text-secondary'>Todos os direitos reservados</p>
         </div>
     );
 }
