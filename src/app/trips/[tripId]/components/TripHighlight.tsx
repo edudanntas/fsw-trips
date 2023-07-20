9546884acfc2e3746231120bf27d058d394d6822
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-20 18:03:34 -0300

    style: adapt highlights

--- src/app/trips/[tripId]/components/TripHighlight.tsx
+++ src/app/trips/[tripId]/components/TripHighlight.tsx
@@ -9,9 +9,9 @@ const TripHighlight = ({ highlights }: TripHighlightsProps) => {
     return (
         <div className="flex flex-col p-5 lg:p-0 lg:mt-12">
             <h2 className="font-semibold text-secondary lg:text-xl">Destaques</h2>
-            <div className="flex flex-wrap gap-y-3 mt-1 lg:mt-5">
+            <div className="flex flex-wrap gap-y-3 mt-4 lg:mt-5">
                 {highlights.map((highlight, index) => (
-                    <div key={highlight} className="flex items-center gap-1 lg:gap-2 w-1/2">
+                    <div key={highlight} className="flex items-center gap-2 lg:gap-2 w-1/2">
                         <Image src="/check-icon.svg" width={25} height={25} alt={highlight} />
                         <p className='text-secondary text-sm lg:text-base'>{highlight}</p>
                     </div>
