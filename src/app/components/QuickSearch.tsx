3c14bc8feb73009b5945e9ef0f985adeaf0f8476
Eduardo Dantas <eduardodanttas@icloud.com>
2023-07-20 16:45:29 -0300

    feat: add more options on quick search

--- src/app/components/QuickSearch.tsx
+++ src/app/components/QuickSearch.tsx
@@ -17,28 +17,35 @@ const QuickSearch = () => {
                         <Image width={40} height={40} src='/Hotel.svg' alt='hotel' />
                     </Link>
 
-                    <p className='text- lg:text-base text-grayPrimary'>Hotel</p>
+                    <p className='text-base lg:text-base text-grayPrimary'>Hotel</p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
                     <Link href={'/trips/search?text=fazenda'} className='flex flex-col items-center hover:text-primary transition-all'>
                         <Image width={40} height={40} src='/Farm.svg' alt='fazenda' />
                     </Link>
 
-                    <p className='text- lg:text-base text-grayPrimary'>Fazenda</p>
+                    <p className='text-base lg:text-base text-grayPrimary'>Fazenda</p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
                     <Link href={'/trips/search?text=chalé'} className='flex flex-col items-center hover:text-primary transition-all'>
                         <Image width={40} height={40} src='/Cabin.svg' alt='cottage' />
                     </Link>
 
-                    <p className='text- lg:text-base text-grayPrimary'>Chalé</p>
+                    <p className='text-base lg:text-base text-grayPrimary'>Chalé</p>
                 </div>
                 <div className="flex flex-col items-center gap-1">
                     <Link href={'/trips/search?text=pousada'} className='flex flex-col items-center hover:text-primary transition-all'>
                         <Image width={40} height={40} src='/inn.svg' alt='pousada' />
                     </Link>
 
-                    <p className='text- lg:text-base text-grayPrimary'>Pousada</p>
+                    <p className='text-base lg:text-base text-grayPrimary'>Pousada</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                    <Link href={'/trips/search?text=resort'} className='flex flex-col items-center hover:text-primary transition-all'>
+                        <Image width={40} height={40} src='/Resort.svg' alt='resort' />
+                    </Link>
+
+                    <p className='text-base lg:text-base text-grayPrimary'>Resort</p>
                 </div>
 
 
